9e59cef3f352c4e2ac8eb5628e2e331b7da2d438
magr <magrc@tutanota.de>
2023-04-24 13:55:48 +0200

    migrate cloudflare state

--- terraform/cloudflare/versions.tf
+++ terraform/cloudflare/versions.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "http" {
-    address        = "https://gitlab.com/api/v4/projects/42386575/terraform/state/cloudflare"
-    lock_address   = "https://gitlab.com/api/v4/projects/42386575/terraform/state/cloudflare/lock"
+    address        = "https://gitlab.com/api/v4/projects/45321252/terraform/state/cloudflare"
+    lock_address   = "https://gitlab.com/api/v4/projects/45321252/terraform/state/cloudflare/lock"
     lock_method    = "POST"
-    unlock_address = "https://gitlab.com/api/v4/projects/42386575/terraform/state/cloudflare/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/45321252/terraform/state/cloudflare/lock"
     unlock_method  = "DELETE"
   }
 }
