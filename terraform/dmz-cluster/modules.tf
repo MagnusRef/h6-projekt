661f1b11079e7d8d96a99374f55b087380ad4f3a
magr <magrc@tutanota.de>
2023-04-26 19:40:05 +0200

    dmz cluster

---
+++ terraform/dmz-cluster/modules.tf
@@ -0,0 +1,79 @@
+module "cluster" {
+  source = "../modules/cluster"
+  vsphere = {
+    cluster    = "BAL-CLUST-P01"
+    datacenter = "BAL-DATAC-P01"
+    portgroup  = "VLAN30"
+    datastore  = "vsanDatastore"
+    template   = "flatcar-linux-stable-template"
+    folder     = "DMZ Kubernetes Cluster"
+  }
+  nodes = [
+    {
+      hostname = "BAL-CPL-DZ01"
+      fqdn     = "bal-cpl-dz01.malumn.local"
+      vcpu     = 2
+      memory   = 4048
+      ipv4     = "10.30.0.10/16"
+      dns1     = "10.20.0.4"
+      dns2     = "10.20.0.5"
+      gw       = "10.30.0.3"
+      size     = 45
+    },
+    {
+      hostname = "BAL-CPL-DZ02"
+      fqdn     = "bal-cpl-dz02.malumn.local"
+      vcpu     = 2
+      memory   = 4048
+      ipv4     = "10.30.0.11/16"
+      dns1     = "10.20.0.4"
+      dns2     = "10.20.0.5"
+      gw       = "10.30.0.3"
+      size     = 45
+    },
+    {
+      hostname = "BAL-CPL-DZ03"
+      fqdn     = "bal-cpl-dz03.malumn.local"
+      vcpu     = 2
+      memory   = 4048
+      ipv4     = "10.30.0.12/16"
+      dns1     = "10.20.0.4"
+      dns2     = "10.20.0.5"
+      gw       = "10.30.0.3"
+      size     = 45
+    },
+    {
+      hostname = "BAL-WRK-DZ01"
+      fqdn     = "bal-wrk-dz01.malumn.local"
+      vcpu     = 4
+      memory   = 8192
+      ipv4     = "10.30.0.20/16"
+      dns1     = "10.20.0.4"
+      dns2     = "10.20.0.5"
+      gw       = "10.30.0.3"
+      size     = 45
+    },
+    {
+      hostname = "BAL-WRK-DZ02"
+      fqdn     = "bal-wrk-dz02.malumn.local"
+      vcpu     = 4
+      memory   = 8192
+      ipv4     = "10.30.0.21/16"
+      dns1     = "10.20.0.4"
+      dns2     = "10.20.0.5"
+      gw       = "10.30.0.3"
+      size     = 45
+    },
+    {
+      hostname = "BAL-WRK-DZ03"
+      fqdn     = "bal-wrk-dz03.malumn.local"
+      vcpu     = 4
+      memory   = 8192
+      ipv4     = "10.30.0.22/16"
+      dns1     = "10.20.0.4"
+      dns2     = "10.20.0.5"
+      gw       = "10.30.0.3"
+      size     = 45
+    },
+  ]
+}
